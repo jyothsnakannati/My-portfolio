4d781ab6ed37dbe4eec5f7c263fb8dac9610fd8e
jyothsnakannati <jyoshnadevikannati@gmail.com>
2024-12-30 12:40:05 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+var typed = new Typed(".text", {
+    strings: ["","Frontend Developer", " Backend Developer ", "Fullstack Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 100,
+    loop: true
+});
+
+
